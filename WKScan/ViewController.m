7e07cc1fecb55fa75b59d4c9ac60ae83734ae2c2
Lishengmin <707159570@qq.com>
2017-08-31 14:47:22 +0800

    Initial Commit

---
+++ WKScan/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WKScan
+//
+//  Created by 李盛民 on 2017/8/31.
+//  Copyright © 2017年 李盛民. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ WKScan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WKScan
+//
+//  Created by 李盛民 on 2017/8/31.
+//  Copyright © 2017年 李盛民. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
